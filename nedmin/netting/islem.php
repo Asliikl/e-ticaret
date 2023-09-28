841036f68499cc5ec29d9016a02d15969ca9ce73
Asliikl <asliikiel9@gmail.com>
2023-09-28 20:01:26 +0300

    ayar sayfası update işlemi gerçekleştirildi

---
+++ nedmin/netting/islem.php
@@ -0,0 +1,31 @@
+<?php 
+include 'baglan.php';
+
+if(isset($_POST['genelayarkaydet'])){
+
+    //güncelleme işlemleri
+    $ayarkaydet=$db->prepare("UPDATE ayar SET
+    ayar_title=:ayar_title,
+    ayar_description=:ayar_description,
+    ayar_keywords=:ayar_keywords,
+    ayar_author=:ayar_author 
+    WHERE ayar_id=0
+    ");
+    $update=$ayarkaydet->execute(array(
+        'ayar_title'=>$_POST['ayar_title'],
+        'ayar_description'=>$_POST['ayar_description'],
+        'ayar_keywords'=>$_POST['ayar_keywords'],
+        'ayar_author'=>$_POST['ayar_author'] 
+    ));
+
+    if($update){
+        header("Location:../production/genel-ayar.php?durum=ok");
+    }
+    else{
+        header("Location:../production/genel-ayar.php?durum=no");
+    }
+}
+
+
+
+?>
